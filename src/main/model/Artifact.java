eeaad530bffdbb8c52db0a03bf05bb8996a69553
Dian <dian0404@students.cs.ubc.ca>
2026-07-14 01:31:21 -0700

    Added implementation for Artifact class

--- src/main/model/Artifact.java
+++ src/main/model/Artifact.java
@@ -7,39 +7,52 @@ public class Artifact {
     private String description;
     private int rating;
 
+    // REQUIRES: rating is between 0 and 5, inclusive
     // EFFECTS: constructs an artifact with a name, museum, description and rating
     public Artifact(String name, String museum, String description, int rating) {
-
+        this.name = name;
+        this.museum = museum;
+        this.description = description;
+        this.rating = rating;
     }
 
     // EFFECTS: returns the name of the artifact
     public String getName() {
-        return null;
+        return name;
     }
 
     // EFFECTS: returns the museum of the artifact
     public String getMuseum() {
-        return null;
+        return museum;
     }
 
     // EFFECTS: returns the description of the artifact
     public String getDescription() {
-        return null;
+        return description;
     }
 
     // EFFECTS: returns the rating of the artifact
     public int getRating() {
-        return 0;
+        return rating;
     }
 
     // EFFECTS: returns true if the artifact has a 5-star rating
     // false otherwise
     public boolean isFiveStar() {
-        return false;
+        if (this.rating == 5) {
+            return true;
+        } else {
+            return false;
     }
+}
 
     // EFFECTS: returns true if the artifact belongs to the specified museum
     // false otherwise
     public boolean belongsToMuseum(String museumName) {
-        return false;
+        if (this.museum.equals(museumName)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
