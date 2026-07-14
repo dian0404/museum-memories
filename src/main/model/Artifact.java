e106dc7c914271a21cfe0daf377e63d397824df8
Dian <dian0404@students.cs.ubc.ca>
2026-07-13 22:29:33 -0700

    Added specification of Artifact class

--- src/main/model/Artifact.java
+++ src/main/model/Artifact.java
@@ -1,11 +1,45 @@
-// X class
+// Represents an artifact with a name, museum, description, and rating.
 package model;
 
-
 public class Artifact {
-    int other;
+    private String name;
+    private String museum;
+    private String description;
+    private int rating;
 
-    public Artifact() {
-        other = 4;
+    // EFFECTS: constructs an artifact with a name, museum, description and rating
+    public Artifact(String name, String museum, String description, int rating) {
+
     }
+
+    // EFFECTS: returns the name of the artifact
+    public String getName() {
+        return null;
+    }
+
+    // EFFECTS: returns the museum of the artifact
+    public String getMuseum() {
+        return null;
+    }
+
+    // EFFECTS: returns the description of the artifact
+    public String getDescription() {
+        return null;
+    }
+
+    // EFFECTS: returns the rating of the artifact
+    public int getRating() {
+        return 0;
+    }
+
+    // EFFECTS: returns true if the artifact has a 5-star rating
+    // false otherwise
+    public boolean isFiveStar() {
+        return false;
+    }
+
+    // EFFECTS: returns true if the artifact belongs to the specified museum
+    // false otherwise
+    public boolean belongsToMuseum(String museumName) {
+        return false;
 }
