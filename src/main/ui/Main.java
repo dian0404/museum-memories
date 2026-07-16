ce47d85b41a8a8f7db4108eacef9e234db3613ef
Dian <dian0404@students.cs.ubc.ca>
2026-07-15 17:28:05 -0700

    Added missed specification for main method

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,8 @@
+// Represents the entry point for the artifact management application.
+
 package ui;
 
+//EFFECTS: starts the artifact console application
 public class Main {
     public static void main(String[] args) {
         new ArtifactConsole();
