ca47ee90f943f2997548c8541467fecf898528af
Dian <dian0404@students.cs.ubc.ca>
2026-07-21 18:55:48 -0700

    Implement JSONWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,15 +1,14 @@
-// Represents a writer that writes a JSON representation of an artifact collection to a file
 package persistence;
 
-import model.Artifact;
 import model.ArtifactCollection;
 
 import org.json.JSONObject;
 
-
 import java.io.*;
 
 // Code in this class is based on the JsonSerializationDemo provided by the UBC CPSC 210 course
+
+// Represents a writer that writes a JSON representation of an artifact collection to a file
 public class JsonWriter {
     private static final int TAB = 4;
     private PrintWriter writer;
@@ -17,31 +16,32 @@ public class JsonWriter {
 
     // EFFECTS: constructs a writer to write to the destination file
     public JsonWriter(String destination) {
-        // stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens the writer;
     // throws FileNotFoundException if the destination file cannot be opened
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes a JSON representation of artifactCollection to file
     public void write(ArtifactCollection artifactCollection) {
-        // stub
+        JSONObject json = artifactCollection.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes the writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes the given JSON string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
