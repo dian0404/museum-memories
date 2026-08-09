395a42bf2b02fdc6309811d28a949fe53368e53b
Dian <dian0404@students.cs.ubc.ca>
2026-08-09 11:30:04 -0700

    Added specification

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -1,64 +1,76 @@
-package ca.ubc.cpsc210.alarm.model;
+package model;
 
 import java.util.Calendar;
 import java.util.Date;
 
+// Code copied from the Event class in the CPSC 210 Alarm System application.
 
-/**
- * Represents an alarm system event.
- */
+// Represents an event with a description and the date and time it was logged
 public class Event {
-	private static final int HASH_CONSTANT = 13;
-	private Date dateLogged;
-	private String description;
-	
-	/**
-	 * Creates an event with the given description
-	 * and the current date/time stamp.
-	 * @param description  a description of the event
-	 */
-	public Event(String description) {
-		dateLogged = Calendar.getInstance().getTime();
-		this.description = description;
-	}
-	
-	/**
-	 * Gets the date of this event (includes time).
-	 * @return  the date of the event
-	 */
-	public Date getDate() {
-		return dateLogged;
-	}
-	
-	/**
-	 * Gets the description of this event.
-	 * @return  the description of the event
-	 */
-	public String getDescription () {
-		return description;
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		if (other == null)
-			return false;
-		
-		if (other.getClass() != this.getClass())
-			return false;
-		
-		Event otherEvent = (Event) other;
-		
-		return (this.dateLogged.equals(otherEvent.dateLogged) &&
-				this.description.equals(otherEvent.description));
-	}
-	
-	@Override
-	public int hashCode() {
-		return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
-	}
-	
-	@Override
-	public String toString() {
-		return dateLogged.toString() + "\n" + description;
-	}
+    private static final int HASH_CONSTANT = 13;
+    private Date dateLogged;
+    private String description;
+
+    /**
+     * Creates an event with the given description
+     * and the current date/time stamp.
+     * 
+     * @param description a description of the event
+     */
+
+    // EFFECTS: constructs an event with the given description and
+    // the current date and time
+    public Event(String description) {
+        dateLogged = Calendar.getInstance().getTime();
+        this.description = description;
+    }
+
+    /**
+     * Gets the date of this event (includes time).
+     * 
+     * @return the date of the event
+     */
+    public Date getDate() {
+        return dateLogged;
+    }
+
+    /**
+     * Gets the description of this event.
+     * 
+     * @return the description of the event
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    // EFFECTS: returns true if other is an Event with the same date,
+    // time and description as this event; otherwise returns false
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+
+        return (this.dateLogged.equals(otherEvent.dateLogged)
+                && this.description.equals(otherEvent.description));
+    }
+
+    // EFFECTS: returns the hash code for this event
+    @Override
+    public int hashCode() {
+        return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
+    }
+
+    // EFFECTS: returns a string representation of this event containing
+    // its date, time and description
+    @Override
+    public String toString() {
+        return dateLogged.toString() + "\n" + description;
+    }
 }
--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -1,59 +1,67 @@
-package ca.ubc.cpsc210.alarm.model;
+package model;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-/**
- * Represents a log of alarm system events.
- * We use the Singleton Design Pattern to ensure that there is only
- * one EventLog in the system and that the system has global access
- * to the single instance of the EventLog.
- */
+// Code copied from the EventLog class in the CPSC 210 Alarm System application.
+
+// Represents a log of events using the Singleton Design Pattern
 public class EventLog implements Iterable<Event> {
-	/** the only EventLog in the system (Singleton Design Pattern) */
-	private static EventLog theLog;
-	private Collection<Event> events;
-	
-	/** 
-	 * Prevent external construction.
-	 * (Singleton Design Pattern).
-	 */
-	private EventLog() {
-		events = new ArrayList<Event>();
-	}
-	
-	/**
-	 * Gets instance of EventLog - creates it
-	 * if it doesn't already exist.
-	 * (Singleton Design Pattern)
-	 * @return  instance of EventLog
-	 */
-	public static EventLog getInstance() {
-		if (theLog == null)
-			theLog = new EventLog();
-		
-		return theLog;
-	}
-	
-	/**
-	 * Adds an event to the event log.
-	 * @param e the event to be added
-	 */
-	public void logEvent(Event e) {
-		events.add(e);
-	}
-	
-	/**
-	 * Clears the event log and logs the event.
-	 */
-	public void clear() {
-		events.clear();
-		logEvent(new Event("Event log cleared."));
-	}
-	
-	@Override
-	public Iterator<Event> iterator() {
-		return events.iterator();
-	}
+    /** the only EventLog in the system (Singleton Design Pattern) */
+    private static EventLog theLog;
+    private Collection<Event> events;
+
+    // EFFECTS: constructs an empty event log
+    private EventLog() {
+        events = new ArrayList<Event>();
+    }
+
+    /**
+     * Gets instance of EventLog - creates it
+     * if it doesn't already exist.
+     * (Singleton Design Pattern)
+     * 
+     * @return instance of EventLog
+     */
+
+    // EFFECTS: returns the single instance of EventLog;
+    // creates it first if it does not already exist
+    public static EventLog getInstance() {
+        if (theLog == null) {
+            theLog = new EventLog();
+        }
+
+        return theLog;
+    }
+
+    /**
+     * Adds an event to the event log.
+     * 
+     * @param e the event to be added
+     */
+
+    // MODIFIES: this
+    // EFFECTS: adds e to this event log
+    public void logEvent(Event e) {
+        events.add(e);
+    }
+
+    /**
+     * Clears the event log and logs the event.
+     */
+
+    // MODIFIES: this
+    // EFFECTS: clears this event log and logs an event indicating
+    // that the event log was cleared
+    public void clear() {
+        events.clear();
+        logEvent(new Event("Event log cleared."));
+    }
+
+    // EFFECTS: returns an iterator over the events in this event log
+    @Override
+    public Iterator<Event> iterator() {
+        return events.iterator();
+    }
 }
